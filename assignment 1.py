47356ba4ed60e011e6a78374e43eb46c51843540
deep-k7 <kondedeep73@gmail.com>
2026-01-06 00:29:24 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,18 @@
+# TASK 1
+a=int(input("Enter The First Number: "))
+b=int(input("Enter The Second Number: "))
+
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Addition:",c)
+print("Subtraction:",d)
+print("Multiplication:",e)
+print("Division:",f)
+
+# TASK 2
+
+a=input("Enter The First Name: ")
+b=input("Enter The Last Name: ")
+print("Hello,",a,b,"Welcome To The Python Program")
